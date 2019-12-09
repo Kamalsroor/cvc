9277d8afeeb2c7d5d1630948bdd4ab20ce23f59b
kamalsroor <teko.2031@gmail.com>
2019-12-09 13:45:44 +0200

    git push

--- modules/Tour/Views/frontend/blocks/list-tour/index.blade.php
+++ modules/Tour/Views/frontend/blocks/list-tour/index.blade.php
@@ -1,25 +1,27 @@
-<div class="container">
-    <div class="bravo-list-tour">
-        <div class="title">
-            {{$title}}
-        </div>
-        <div class="list-item">
-            @if($style_list === "normal")
-                <div class="row">
-                    @foreach($rows as $row)
-                        <div class="col-lg-3 col-md-6">
+@auth
+    <div class="container">
+        <div class="bravo-list-tour">
+            <div class="title">
+                {{$title}}
+            </div>
+            <div class="list-item">
+                @if($style_list === "normal")
+                    <div class="row">
+                        @foreach($rows as $row)
+                            <div class="col-lg-3 col-md-6">
+                                @include('Tour::frontend.layouts.search.loop-gird')
+                            </div>
+                        @endforeach
+                    </div>
+                @endif
+                @if($style_list === "carousel")
+                    <div class="owl-carousel">
+                        @foreach($rows as $row)
                             @include('Tour::frontend.layouts.search.loop-gird')
-                        </div>
-                    @endforeach
-                </div>
-            @endif
-            @if($style_list === "carousel")
-                <div class="owl-carousel">
-                    @foreach($rows as $row)
-                        @include('Tour::frontend.layouts.search.loop-gird')
-                    @endforeach
-                </div>
-            @endif
+                        @endforeach
+                    </div>
+                @endif
+            </div>
         </div>
     </div>
-</div>
+@endauth
