03f9ba739aae3e27e945663db68b51c75c3d4ce8
kamalsroor <teko.2031@gmail.com>
2019-12-09 16:28:09 +0200

    فثسف

--- modules/Tour/Views/frontend/layouts/search/loop-gird.blade.php
+++ modules/Tour/Views/frontend/layouts/search/loop-gird.blade.php
@@ -26,46 +26,18 @@
     </div>
     <div class="item-title">
         <a @if(!empty($blank)) target="_blank" @endif href="{{$row->getDetailUrl()}}">
-            {{$row->title}}
+            <h4>
+                {{$row->title}}
+            </h4>
         </a>
         {!! $row->content !!}
 
     </div>
-    <?php
-    $reviewData = $row->getScoreReview();
-    $score_total = $reviewData['score_total'];
-    ?>
-    <div class="service-review tour-review-{{$score_total}}">
-        <div class="list-star">
-            <ul class="booking-item-rating-stars">
-                <li><i class="fa fa-star-o"></i></li>
-                <li><i class="fa fa-star-o"></i></li>
-                <li><i class="fa fa-star-o"></i></li>
-                <li><i class="fa fa-star-o"></i></li>
-                <li><i class="fa fa-star-o"></i></li>
-            </ul>
-            <div class="booking-item-rating-stars-active" style="width: {{  $score_total * 2 * 10 ?? 0  }}%">
-                <ul class="booking-item-rating-stars">
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                    <li><i class="fa fa-star"></i></li>
-                </ul>
-            </div>
-        </div>
-        <span class="review">
-            @if($reviewData['total_review'] > 1)
-                {{ __(":number Reviews",["number"=>$reviewData['total_review'] ]) }}
-            @else
-                {{ __(":number Review",["number"=>$reviewData['total_review'] ]) }}
-            @endif
-        </span>
-    </div>
+
     <div class="info">
         <div class="duration">
             <i class="icofont-wall-clock"></i>
-            {{$row->duration}} {{__("hour")}}
+            {{$row->duration}} {{__("Day")}}
         </div>
         <div class="g-price">
             <div class="prefix">
