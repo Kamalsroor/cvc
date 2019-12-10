f650b48b0de01fe172e98b42b067e1b7857b4b9b
kamalsroor <teko.2031@gmail.com>
2019-12-10 15:12:23 +0200

    gdgdg

--- modules/Hotel/Views/frontend/blocks/list-hotel/index.blade.php
+++ modules/Hotel/Views/frontend/blocks/list-hotel/index.blade.php
@@ -1,27 +1,6 @@
-{{-- @auth
-    <div class="container">
-        <div class="bravo-list-tour">
-            <div class="title">
-                {{$name}}
-            </div>
-            <div class="list-item">
-                @if($style_list === "normal")
-                    <div class="row">
-                        @foreach($rows as $row)
-                            <div class="col-lg-3 col-md-6">
-                                @include('Hotel::frontend.layouts.search.loop-gird')
-                            </div>
-                        @endforeach
-                    </div>
-                @endif
-                @if($style_list === "carousel")
-                    <div class="owl-carousel">
-                        @foreach($rows as $row)
-                            @include('Hotel::frontend.layouts.search.loop-gird')
-                        @endforeach
-                    </div>
-                @endif
-            </div>
+<div class="container">
+    <div class="bravo-list-tour">
+
         </div>
     </div>
-@endauth --}}
+</div>
