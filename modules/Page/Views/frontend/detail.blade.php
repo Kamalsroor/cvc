818ca9b8bae0b5b52305c7e4b2b59159853ead07
kamalsroor <teko.2031@gmail.com>
2019-12-10 16:05:40 +0200

    dsgsdg

--- modules/Page/Views/frontend/detail.blade.php
+++ modules/Page/Views/frontend/detail.blade.php
@@ -6,7 +6,7 @@
         </div>
 
         <div class="container " style="padding-top: 40px;padding-bottom: 40px;">
-            <h1>{{$row->title}}</h1>
+            {{-- <h1>{{$row->title}}</h1> --}}
             <div class="blog-content">
                 {!! $row->content !!}
             </div>
