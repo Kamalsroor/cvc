e38c3d247aff63b7b5f95189f7ac29b42759a1bd
kamalsroor <teko.2031@gmail.com>
2019-12-10 15:08:02 +0200

    add list_hotels to template

--- config/template.php
+++ config/template.php
@@ -5,6 +5,7 @@ return [
 //        'column'=>"\\Modules\\Template\\Blocks\\Column",
         'text'=>"\\Modules\\Template\\Blocks\\Text",
         'list_tours'=>"\\Modules\\Tour\\Blocks\\ListTours",
+        'list_hotels'=>"\\Modules\\Hotel\\Blocks\\ListHotels",
         'form_search_tour'=>"\\Modules\\Tour\\Blocks\\FormSearchTour",
         'bannar_text'=>"\\Modules\\Tour\\Blocks\\BannarText",
         'list_featured_item'=>"\\Modules\\Tour\\Blocks\\ListFeaturedItem",
