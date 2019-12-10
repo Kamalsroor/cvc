96e75bf108dd54a992c2223b37ccdacf553b4ac7
kamalsroor <teko.2031@gmail.com>
2019-12-10 15:10:39 +0200

    gdsgs

--- modules/Hotel/Views/frontend/blocks/list-hotel/index.blade.php
+++ modules/Hotel/Views/frontend/blocks/list-hotel/index.blade.php
@@ -5,22 +5,7 @@
                 {{$name}}
             </div>
             <div class="list-item">
-                @if($style_list === "normal")
-                    <div class="row">
-                        @foreach($rows as $row)
-                            <div class="col-lg-3 col-md-6">
-                                @include('Hotel::frontend.layouts.search.loop-gird')
-                            </div>
-                        @endforeach
-                    </div>
-                @endif
-                @if($style_list === "carousel")
-                    <div class="owl-carousel">
-                        @foreach($rows as $row)
-                            @include('Hotel::frontend.layouts.search.loop-gird')
-                        @endforeach
-                    </div>
-                @endif
+
             </div>
         </div>
     </div>
